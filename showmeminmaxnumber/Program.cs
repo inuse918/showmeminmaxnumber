fd777f46cc24850fce6c588b56df52c435a7a840
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-12 12:35:55 +0900

    숫자 입력

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] input = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                input[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("----------------------");
+            
+
+            //Console.WriteLine(num);
         }
     }
 }
