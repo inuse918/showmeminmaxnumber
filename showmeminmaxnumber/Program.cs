f98bb56c291831e75286bdab1d06ab1f3083a19f
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-12 12:44:01 +0900

    가장 작은 수, 가장 큰 수 출력

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -12,14 +12,29 @@ namespace showmeminmaxnumber
         static void Main(string[] args)
         {
             int[] input = new int[5];
-            for(int i = 0; i < 5; i++)
+            int temp;
+            for(int i = 0; i < input.Length; i++)
             {
                 Console.Write("숫자를 입력해주세요: ");
                 input[i] = int.Parse(Console.ReadLine());
             }
             Console.WriteLine("----------------------");
             
+            for(int i = 0; i < input.Length-1; i++)
+            {
+                for(int j = i+1; j < input.Length; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        temp = input[i];
+                        input[i] = input[j];
+                        input[j] = temp; 
+                    }
+                }
+            }
 
+            Console.WriteLine("가장 큰 수: "+input[4]);
+            Console.WriteLine("가장 작은 수: " + input[0]);
             //Console.WriteLine(num);
         }
     }
